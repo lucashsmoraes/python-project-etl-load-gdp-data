d40109b99a45047611fb4c51d8e5d7cfb6f9aa34
lucashsmoraes <lucashsmoraes@gmail.com>
2024-07-07 13:57:42 -0300

    adicionando variaveis

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -4,6 +4,18 @@ import pandas as pd
 import numpy as np
 import sqlite3
 from datetime import datetime
+'''
+1 - URL 
+2 - table_attribs: Os atributos ou nomes de colunas para o dataframe armazenado com uma lista.
+3 - db_name: 'World_Economies.db'
+4 - table_name: 'Countries_by_GDP'
+5 - csv_path: 'Countries_by_GDP.csv'
+'''
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ["Country", "GDP_USD_millions"]
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = './Countries_by_GDP.csv'
 
 '''
  Função responsável por extrair informações necessárias do site e savar
