2669c7053258f3e5245a778a33fca7569ad5a2ba
lucashsmoraes <lucashsmoraes@gmail.com>
2024-07-07 15:26:07 -0300

    Implementando função para criar arquivo de log e finalizando projeto

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -81,4 +81,11 @@ def run_query(query_statement, sql_connection):
 Função responsável por registrar a mensagem logada em um determinado
 estágio da execução do código em um arquivo de log. A função não retorna nada
 '''
-# def log_progress(message):
+
+
+def log_progress(message):
+    timestamp_format = '%Y-%n-%d-%H:%M:%S'  # Year-Monthname-Day-Hour-Minute-Second
+    now = datetime.now()  # get current timestamp
+    timestamp = now.strftime(timestamp_format)
+    with open("./etl_project_gdp.txt", "a") as f:
+        f.write(timestamp + ' : ' + message + '\n')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from etl_project_gdp import extract, transform, load_to_csv, load_to_db, run_query
+from etl_project_gdp import extract, transform, load_to_csv, load_to_db, run_query, log_progress
 import sqlite3
 
 '''
@@ -15,11 +15,25 @@ table_name = 'Countries_by_GDP'
 csv_path = './Countries_by_GDP.csv'
 
 if __name__ == '__main__':
+    log_progress('Iniciando processo ETL')
     df = extract(url, table_attribs)
+
+    log_progress('Extração de dados concluída. Iniciando o processo de transformação')
     df = transform(df)
+
+    log_progress('Transformação de dados concluída. Iniciando processo de carregamento')
     load_to_csv(df, csv_path)
+
+    log_progress('Dados salvos em arquivo CSV. Iniciando conecção com banco de dados')
     sql_connection = sqlite3.connect('World_Economies.db')
+
+    log_progress('Conecção SQL Finalizada, Iniciando carregamento dado como tabela no banco de dados')
     load_to_db(df, sql_connection, table_name)
+
+    log_progress('Dados carregados com sucesso. Iniciando consulta na tabela')
     query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 28780"
     run_query(query_statement, sql_connection)
+
+    log_progress('Processo finalizado com sucesso')
+    sql_connection.close()
 
